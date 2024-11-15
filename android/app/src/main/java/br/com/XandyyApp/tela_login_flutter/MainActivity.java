11ea6df9a02697816903c6b9e76d959b38aff4d6
Alexandre <alexandre.s.o1994@hotmail.com>
2024-11-15 18:35:10 -0300

    comeco projeto tela de login

---
+++ android/app/src/main/java/br/com/XandyyApp/tela_login_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.XandyyApp.tela_login_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:tela_login_flutter/login_ui.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: LoginUi(),
+    );
+  }
+}
